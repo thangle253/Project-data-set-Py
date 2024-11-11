0874ead9cb13d18ea20db883634e865d72974fa2
junUTE <vuquoctrung0@gmail.com>
2024-11-11 21:46:42 +0700

    Update Trung.py
    chỉnh sửa code phần biểu đồ pairplot

--- Trung.py
+++ Trung.py
@@ -7,8 +7,8 @@ df = pd.read_csv('cars.csv')
 # Kiểm tra một số thông tin cơ bản của dữ liệu
 df.head()
 df.describe()
-df.info()
 df.isnull().sum()
+df.info()
 
 # Mã hóa các biến phân loại thành các giá trị số
 # Sử dụng pandas để mã hóa cột phân loại
@@ -16,10 +16,12 @@ encoded_df = df.copy()
 for column in df.select_dtypes(include=['object']).columns:
     encoded_df[column] = encoded_df[column].astype('category').cat.codes
 
-# # Các mối quan hệ từng cặp (sử dụng dữ liệu đã được mã hóa nhãn)
-# plt.figure(figsize=(10, 8))
-# sns.pairplot(encoded_df, hue='class', palette='coolwarm')
-# plt.show()
+# Các mối quan hệ từng cặp (sử dụng dữ liệu đã được mã hóa nhãn)
+class_mapping = dict(enumerate(df['class'].astype('category').cat.categories))
+print(class_mapping)    
+plt.figure(figsize=(4, 3))
+sns.pairplot(encoded_df, hue='class', palette="plasma")
+plt.show()
 
 # Phân tích sự cân bằng các lớp xe 
 plt.figure(figsize=(8, 6))
