a28094238ab0b5a08ef94903fe345376f9442825
junUTE <vuquoctrung0@gmail.com>
2024-11-11 21:03:44 +0700

    Update Trung.py
    sửa code, thêm chú thích

--- Trung.py
+++ Trung.py
@@ -1,12 +1,14 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+# Đọc dữ liệu từ file CSV
 df = pd.read_csv('cars.csv')
+
+# Kiểm tra một số thông tin cơ bản của dữ liệu
 df.head()
 df.describe()
-df.isnull().sum()
 df.info()
+df.isnull().sum()
 
 # Mã hóa các biến phân loại thành các giá trị số
 # Sử dụng pandas để mã hóa cột phân loại
@@ -14,15 +16,16 @@ encoded_df = df.copy()
 for column in df.select_dtypes(include=['object']).columns:
     encoded_df[column] = encoded_df[column].astype('category').cat.codes
 
-# Các mối quan hệ từng cặp (sử dụng dữ liệu đã được mã hóa nhãn)
-plt.figure(figsize=(10, 8))
-sns.pairplot(encoded_df, hue='class', palette='coolwarm')
-plt.show()
+# # Các mối quan hệ từng cặp (sử dụng dữ liệu đã được mã hóa nhãn)
+# plt.figure(figsize=(10, 8))
+# sns.pairplot(encoded_df, hue='class', palette='coolwarm')
+# plt.show()
 
-# Phân tích sự cân bằng lớp
+# Phân tích sự cân bằng các lớp xe 
 plt.figure(figsize=(8, 6))
 class_counts = df['class'].value_counts()
 sns.barplot(x=class_counts.index, y=class_counts.values, palette='Set2')
-plt.title('Phân tích sự cân bằng lớp')
-plt.ylabel('Count')
+plt.title('Phân tích sự cân bằng các lớp xe')
+plt.ylabel('Số lượng')
+plt.xlabel('Lớp xe')
 plt.show()
