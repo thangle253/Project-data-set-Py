fa416ce9b70427cb0cf17c29945fc4bf5e1be75c
junUTE <vuquoctrung0@gmail.com>
2024-11-12 21:07:12 +0700

    Update Trung.py
    thay đổi màu biểu đồ pairplot dễ quan sát hơn, hiện thị tên các lớp xe ở phần chú thích

--- Trung.py
+++ Trung.py
@@ -18,9 +18,9 @@ for column in df.select_dtypes(include=['object']).columns:
 
 # Các mối quan hệ từng cặp (sử dụng dữ liệu đã được mã hóa nhãn)
 class_mapping = dict(enumerate(df['class'].astype('category').cat.categories))
-print(class_mapping)    
+encoded_df['class'] = encoded_df['class'].map(class_mapping)
 plt.figure(figsize=(4, 3))
-sns.pairplot(encoded_df, hue='class', palette="plasma")
+sns.pairplot(encoded_df, hue='class', palette="Pastel1")
 plt.show()
 
 # Phân tích sự cân bằng các lớp xe 
